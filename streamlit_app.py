6a25bec6463cf24f6f13fa2a52814f690cf807e3
yuankiki <34342962+yuankiki@users.noreply.github.com>
2023-08-09 11:18:01 +1000

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
